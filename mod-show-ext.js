ca4afcb86d23ac91a1fbe06ed965f1ecf1bff213
ajax <kolbaska205@gmail.com>
2018-09-24 17:48:28 +0600

    jhfgdfd

--- mod-show-ext.js
+++ mod-show-ext.js
@@ -1,56 +1,45 @@
-function extAccessTable(){
-	var file = new SCFile('Subscription');
-	var cont = new SCFile('contacts');
-	cont.doSelect('full.name="'+vars['$recipient']+'"');
-	//var data = [];
-	//var cnt = 0;
-	//var sCR = "\n";
-	//var sHtmlReturn = getCSS();
-	var rc = file.doSelect('subscriber="'+cont['contact.name']+'" and type="Расширенный доступ"');
-	if(rc==RC_SUCCESS){
-		do{
-			//data[cnt] = {id:file['subscriptionID'],serv:file['displayName'],acss:file['acss.name'],right:file['right.name']};
-			//cnt++;
-			vars['$bzs'] = _ins(vars['$bzs'],file['displayName']);
-			vars['$bzsID'] = _ins(vars['$bzsID'],file['serviceName']);
-			vars['$accs'] = _ins(vars['$accs'],file['acss.name']);
-			vars['$accsID'] = _ins(vars['$accsID'],file['acss.id']);
-			vars['$right'] = _ins(vars['$right'],file['right.name']);
-			vars['$rightID'] = _ins(vars['$rightID'],file['right.id']);
-			vars['$subId'] = _ins(vars['$subId'],file['subscriptionID']);
-		}while(file.getNext() == RC_SUCCESS)
+var file = new SCFile('accessRequest');
+for (var i = 0; i <_lng(vars['$recipient']); i++){
+	if(vars['$newRight']!=null){
+		for(var j = 0; j< _lng(vars['$bzs']); j++){
+			file['cis.bzs.id'] = vars['$lo.bzs'][j];
+			file['cis.acss.id'] = vars['$lo.accs'][j];
+			file['cis.right.id'] = vars['$right'][j];
+			file['cis.bzs.name'] = vars['$bzs'][j];
+			file['cis.acss.name'] = vars['$accs'][j];
+			file['cis.right.name'] = vars['$rightNames'][j]
+			file['recipient'] = vars['$recipient'][i];
+			file['category'] = 'Изменение';	//предоставление изменние отзыв
+			file['type'] = 'Расширеный';		//базовый расширеный ролевой
+			file['company'] = vars['$lo.company'];
+			file['date.open'] = _tod();
+			file['oper.open'] = _op();
+			if(vars['$newDate']!=null){
+				file['end.date'] = vars['$newDate'];
+			}
+			file['subscription.id'] = //subid;
+			file['initiator'] = _op();
+			file['description'] = vars['$reason'];
+			var rc = file.doInsert();
+		}
 	}
 }
-	/*sHtmlReturn += "<table class=\"main\">" + sCR;
-	// Table header
-	sHtmlReturn += "<tr><th><div tabindex=\"0\"> Действие </div></th>"
-	sHtmlReturn += "<th><div tabindex=\"0\"> Услуга </div></th>"
-	sHtmlReturn += "<th><div tabindex=\"0\"> Обьект доступа </div></th>"
-	sHtmlReturn += "<th><div tabindex=\"0\"> Право доступа </div></th>";
-	for (var i =0; i<data.length; i++) {
-		var sRowClass = i%2==0 ? "evenRow" : "oddRow";
-			sHtmlReturn += "<tr>";
-			 sHtmlReturn += "<td class=\""+sRowClass+"\" + text-align:center > <a href=\"scactivelink://device:"+data[i]['id']+"\">Изменить</a></td>";
-			if(data[i]['roleid']!=null){
-				sHtmlReturn += "<td class=\""+sRowClass+"\" >"+data[i]['serv']+"</td>";		//ResourceID
-			}
-			else{
-				sHtmlReturn += "<td class=\""+sRowClass+"\" > </td>";
-			}
-			if(data[i]['rolename']!=null){
-				sHtmlReturn += "<td class=\""+sRowClass+"\" >"+data[i]['acss']+"</td>";	//SerialNumber0
-			}
-			else{
-				sHtmlReturn += "<td class=\""+sRowClass+"\" > </td>";
-			}
-			if(data[i]['rolename']!=null){
-				sHtmlReturn += "<td class=\""+sRowClass+"\" >"+data[i]['right']+"</td>";	//SerialNumber0
-			}
-			else{
-				sHtmlReturn += "<td class=\""+sRowClass+"\" > </td>";
-			}
-		sHtmlReturn += "<tr>";
+for(var i = 0; i <_lng(vars['$recipient']); i++){
+	if(_lng(vars['$role'])>0){
+		for(var j = 0; j< _lng(vars['$bzs']); j++){
+			file['recipient'] = vars['$recipient'][i];
+			file['category'] = 'Изменение';	//предоставление изменние отзыв
+			file['type'] = 'Ролевой';		//базовый расширеный ролевой
+			file['company'] = vars['$lo.company'];
+			file['date.open'] = _tod();
+			file['oper.open'] = _op();
+			file['end.date'] = vars['$newDate'];
+			file['subscription.id'] = //subid;
+			file['initiator'] = _op();
+			file['description'] = vars['$reason'];
+			file['role.ci.name'] = vars['$role'][j]
+			file['role.logical.name'] = vars['$lo.role'][j];
+			var rc = file.doInsert();
+		}
 	}
-	sHtmlReturn += "</table>" + sCR;
-	return sHtmlReturn;
-}*/
+}
